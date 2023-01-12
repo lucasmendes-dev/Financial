6f3fdb4f79f0c10062bf8e6a16bc4cfb2b0bb23c
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2023-01-11 22:26:24 -0300

    assets template and create

--- asset/urls.py
+++ asset/urls.py
@@ -5,9 +5,6 @@ app_name = 'assets'
 
 urlpatterns = [    
     path('', views.asset_index, name='index'),
-    path('create/', views.asset_create, name='create'),
-    path('store/', views.asset_store, name='store'),
-    path('edit/<int:pk>', views.asset_edit, name='edit'),
-    path('update/', views.asset_update, name='update'),
-    path('delete/', views.asset_delete, name='delete'),
+    path('create/', views.asset_create, name='create'),    
+    path('update/<int:id>', views.asset_update, name='update'),    
 ]
--- asset/views.py
+++ asset/views.py
@@ -1,30 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Asset, AssetTransaction
+import requests
 
 
 def asset_index(request):
     
-    return render(request, 'assets.html')
+    assets = Asset.objects.all().filter(user=request.user)    
+    
+    if assets:
+        
+        #iniciando vetores para armazenar dados
+        preco_atual = []    
+        variacao_diaria = []
+        valor_diario = []
+        valor_dia = []        
+        variacao = [] 
+        variacao_total = []
+        
+        #para gerar a url com todos os tickers do usuário
+        key = ""
+        for i, cod in enumerate(assets):
+            if (i + 1) < len(assets):
+                key+= cod.asset_code + "%2C"
+            else:
+                key+= cod.asset_code
+            
+        #requisição da API com os tickers de cada ativo do usuário
+        url =  f'https://brapi.dev/api/quote/{key}?range=1d&interval=1d&fundamental=false'
+        r = requests.get(url)
+        data = r.json()
+                    
+        #populando os vetores com dados 
+        for valor in data['results']:
+            preco_atual.append(valor['regularMarketPrice'])              
+            variacao_diaria.append(float(f"{valor['regularMarketChangePercent']:.2f}"))      
+            valor_diario.append(float(f"{valor['regularMarketChange']:.2f}"))      
+            
+            
+        #realizando operações para tratar os dados antes de ir pro template
+        my_list = zip(assets, preco_atual, valor_diario)  
+                           
+        for asset, cotacao, valor_diario in my_list:
+            
+            result_percent = (cotacao - asset.average_price) * 100 / asset.average_price  #cálculo da variação em % (Variação)
+            result_variacao = (cotacao - asset.average_price) * asset.asset_qty #calculo da variação total em R$ (Variação Total)
+            result_valor_dia = valor_diario * asset.asset_qty #calculo da variação diária em R$ (Variação/dia)
+                        
+            variacao.append(float(f'{result_percent:.2f}'))
+            variacao_total.append(float(f'{result_variacao:.2f}'))
+            valor_dia.append(float(f'{result_valor_dia:.2f}'))
+            
+            
+        #zip de todas as informações para enviar pro template    
+        table_list = zip(assets, preco_atual, variacao_diaria, valor_dia, variacao, variacao_total)
+        
+        context = {
+            'table_list': table_list
+        }
+                    
+                
+                
+        return render(request, 'index.html', context)
+    
+    else:
+        return render(request, 'index.html')
 
 
 def asset_create(request):
-    pass
-
-
-def asset_store(request):
-    pass
-
-
-def asset_edit(request):
-    pass
+    
+    if request.method == "POST":
+        
+        asset = Asset(
+            asset_type = request.POST['asset_type'],
+            asset_name = request.POST['asset_name'],
+            asset_code = request.POST['asset_code'],
+            asset_qty = request.POST['asset_qty'],
+            average_price = request.POST['average_price'],
+            status = request.POST['status'],
+            account_id = 4,
+            user = request.user
+        )
+        
+        asset.save()
+        return redirect('assets:index')
+        
+    else:        
+        return render(request, 'index.html')
 
 
 def asset_update(request):
     pass
 
 
-def asset_delete(request):
-    pass
 
 
 #Auxiliar functions ↓
